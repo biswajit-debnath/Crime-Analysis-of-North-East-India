c353b18bd8d7f4887de95298e13f447c50170a7b
biswajit-debnath <biswajitdebnath405@gmail.com>
2020-04-03 02:30:41 +0530

    clusters made on a state

---
+++ Test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 23:20:01 2020
+
+@author: Biswajit Debnath
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np;
+import pandas as pd;
+
+df= pd.read_csv('Manipur.csv');
+
